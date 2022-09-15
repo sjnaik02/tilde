b7b5d3e9ed5902f8db30e917ccd4fde495b3e99b
Shourya Naik <sjnaik02@gmail.com>
2022-09-15 10:13:21 +0530

    added sign in/out button

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,53 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 function Layout({ children }) {
   const router = useRouter();
   const { pathname } = router;
-  console.log(pathname);
+  const session = useSession();
+  let userString = session.data ? session.data.user.name : "not signed in";
   return (
     <>
-      <div className="bg-gray-800 font-mono py-4" >
+      <div className="bg-gray-800 font-mono py-4">
         <div className="flex items-center justify-between max-w-4xl mx-auto px-4 lg:px-0">
-          <p className="text-gray-100 text-xl py-2 ">User</p>
-          {/*hide new note button when on create-note screen */
-            pathname !== '/create-note' && ( <button className="w-fit bg-teal-600 text-gray-100 rounded p-2 " > New Note </button>)  }
-         
+          <div className="flex gap-4">
+            <p className="text-gray-100 text-xl py-2 ">{userString}</p>
+            {
+              !session.data && (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={() => signIn()}
+                >
+                  Sign In
+                </button>
+              )
+            }
+            {
+              session.data && (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={() => signOut()}
+                >
+                  Sign Out
+                </button>
+              )
+            }
+          </div>
+          {
+            pathname !== "/create-note" && (
+              <button
+                className="w-fit bg-teal-600 text-gray-100 rounded p-2 "
+                onClick={() => router.push("/create-note")}
+              >
+                New Note
+              </button>
+            )
+          }
         </div>
       </div>
       {children}
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
