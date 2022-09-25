9127ba5fe5dc5e4d26e1ef15c9daa73da512fee5
Shourya Naik <sjnaik02@gmail.com>
2022-09-25 13:46:27 +0530

    button component created

---
+++ components/Button.js
@@ -0,0 +1,15 @@
+//extract all my buttons into custom components
+
+function Button({ children, onClick, className, disabled }) {
+  return (
+    <button
+      className={`mx-2 rounded border-2 border-blue-500 py-2 px-4 font-bold text-blue-500 hover:border-blue-500 hover:bg-blue-500 hover:text-gray-100 disabled:border-gray-400 disabled:text-gray-100 disabled:hover:bg-gray-800 ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
