24bf53589c2178a8942cd5316527d3973835d6e0
Shourya Naik <sjnaik02@gmail.com>
2022-10-14 15:14:40 +0530

    using Header and icons in buttons

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,8 @@ import { useRouter } from "next/router";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import Footer from "./Footer";
+import Header from "./Header";
+import { VscSignIn, VscSignOut, VscAdd } from "react-icons/vsc";
 
 function Layout({ children }) {
   const router = useRouter();
@@ -10,50 +12,7 @@ function Layout({ children }) {
   let userString = session.data ? session.data.user.name : "Not signed in";
   return (
     <>
-      <div className="bg-primary py-4 font-mono">
-        <div className="mx-auto flex max-w-4xl items-center justify-between px-4 lg:px-0">
-          <Link href="/">
-            <a className="text-xl font-bold text-gray-50 hover:text-gray-300">
-              ~Tilde~
-            </a>
-          </Link>
-          <div className="flex">
-            {session.status === "unauthenticated" ? (
-              <p className="py-2 text-xl text-gray-100 ">{userString}</p>
-            ) : (
-              <Link href="/profile">
-                <a className="py-2 text-xl text-gray-100 hover:text-gray-300">
-                  {userString}
-                </a>
-              </Link>
-            )}
-            {!session.data && (
-              <button
-                className="hover:bg-blue-700text-sm mx-4 rounded bg-blue-500 p-2 font-bold text-white"
-                onClick={() => signIn()}
-              >
-                Sign In
-              </button>
-            )}
-            {session.data && (
-              <button
-                className="mx-4 rounded bg-blue-500 p-2 text-sm font-bold text-white hover:bg-blue-700"
-                onClick={() => signOut()}
-              >
-                Sign Out
-              </button>
-            )}
-            {pathname !== "/create-note" && (
-              <button
-                className="w-fit rounded bg-teal-600 p-2 text-sm font-bold text-gray-100 hover:bg-teal-700"
-                onClick={() => router.push("/create-note")}
-              >
-                New Note
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
+      <Header />
       {children}
       <Footer />
     </>
