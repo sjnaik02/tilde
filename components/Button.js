ec1ed0fc02b1e6c1bdeeb8e7b930847b9ab6035f
Shourya Naik <sjnaik02@gmail.com>
2022-10-14 15:14:55 +0530

    updated button to use transition

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,7 @@
 function Button({ children, onClick, className, disabled }) {
   return (
     <button
-      className={`mx-1 rounded border-2 border-blue-500 py-2 px-4 font-bold text-blue-500 hover:border-blue-500 hover:bg-blue-500 hover:text-gray-100 disabled:border-gray-400 disabled:text-gray-100 disabled:hover:bg-gray-800 md:mx-2 ${className}`}
+      className={`mx-1 rounded border-2 border-blue-500 py-2 px-4 font-bold text-blue-500 transition-colors hover:border-blue-500 hover:bg-blue-500 hover:text-gray-100 disabled:border-gray-400 disabled:text-gray-100 disabled:hover:bg-gray-800 md:mx-2 ${className}`}
       onClick={onClick}
       disabled={disabled}
     >
