08200804b3ec283cc5da11535c6fd6ac619149ed
Shourya Naik <sjnaik02@gmail.com>
2022-10-14 14:55:12 +0530

    cards function as links now

--- components/Card.js
+++ components/Card.js
@@ -3,22 +3,21 @@ import Link from "next/link";
 
 function Card({ title, content, id, isPrivate }) {
   return (
-    <div className="rounded-lg border-2 border-blue-800 p-4 font-bold hover:border-blue-500">
-      <h3 className="w-full overflow-hidden overflow-ellipsis whitespace-nowrap py-1 text-xl font-bold">
-        {title}
-      </h3>
-      <p className="w-full overflow-hidden overflow-ellipsis whitespace-nowrap py-1 text-sm">
-        {content}
-      </p>
-      <p className=" text-xs text-green-500">
-        {isPrivate ? "Private" : "Public"}
-      </p>
-      <Link href={`/notes/${id}`}>
-        <a className=" text-xs text-blue-500 hover:text-blue-400 hover:underline">
-          View
-        </a>
-      </Link>
-    </div>
+    <Link href={`/notes/${id}`}>
+      <a>
+        <div className="rounded-lg border-2 border-gray-800 p-4 font-bold transition-all hover:border-gray-200">
+          <h3 className="w-full overflow-hidden overflow-ellipsis whitespace-nowrap py-1 text-xl font-bold">
+            {title}
+          </h3>
+          <p className="w-full overflow-hidden overflow-ellipsis whitespace-nowrap py-1 text-sm">
+            {content}
+          </p>
+          <p className=" text-xs text-green-500">
+            {isPrivate ? "Private" : "Public"}
+          </p>
+        </div>
+      </a>
+    </Link>
   );
 }
 
